ab5e9a34b6cb3b0bf96290a27c258b55e60651fe
Soham Nimale <soham.sachin.nimale@gmail.com>
2022-10-25 20:37:33 +0530

    added easy question - hash set - intersection and union of array

---
+++ src/easyQ/HashSetQuestions.java
@@ -0,0 +1,33 @@
+package easyQ;
+
+import java.util.HashSet;
+import java.math.*;
+
+public class HashSetQuestions {
+	public static void main(String args[]) {
+		
+	}
+	
+	public static HashSet<Integer> setUnion(HashSet<Integer> set1, HashSet<Integer> set2) {
+		HashSet<Integer> union = new HashSet<>(set1.size()+set2.size());
+		for(int e : set1) {
+			union.add(e);
+		}
+		for(int e : set2) {
+			union.add(e);
+		}
+		return union;
+	}
+	
+	public static HashSet<Integer> setIntersection(HashSet<Integer> set1, HashSet<Integer> set2) {
+		HashSet<Integer> intersection = new HashSet<>(Math.min(set1.size(), set2.size()));
+		for (int e : set1) {
+			if(set2.contains(e)) {
+				intersection.add(e);
+			}
+		}
+		return intersection;
+		
+	}
+	
+}
