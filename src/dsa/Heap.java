0f84cf2bdec34ca3aeb670625dab9494b2241ef7
Soham Nimale <soham.sachin.nimale@gmail.com>
2022-10-29 21:46:45 +0530

    added heapify, delete, buildHeap and heapSort

--- src/dsa/Heap.java
+++ src/dsa/Heap.java
@@ -17,15 +17,12 @@ public class Heap {
 	static int n1;
 	
 	public static void main(String args[]) {
-		int[] heap1 = new int[15];
-		for (int i = 0; i < 9; i++) {
-			heap1[i] = 9 - i;
-			n1++;
-		}
-		insert(heap1, 8);
+		int[] heap1 = {9, 8, 1, 3, 5, 2, 34 , 3555, 5, 6};
+		n1=10;
+		heapSort(heap1);
 		printHeap(heap1);
 	}
-
+	
 	public static void swap(int[] arr, int a, int b) {
 		if (arr[a]==arr[b]) {
 			return;
@@ -50,20 +47,52 @@ public class Heap {
 	
 	
 	// insert element in heap
-	public static void insert(int[] heap1, int k) {
-		heap1[n1] = k;
+	public static void insert(int[] heap, int k) {
+		heap[n1] = k;
 		n1++;
 		int index = n1;
 		while (index > 1) {
 			int parent = index / 2;
-			if (heap1[index - 1] > heap1[parent - 1]) {
-				swap(heap1, parent - 1, index - 1);
+			if (heap[index - 1] > heap[parent - 1]) {
+				swap(heap, parent - 1, index - 1);
 				index = parent;
 			} else {
 				return;
 			}
 		}
 
+	}
+	
+	public static void heapify(int[] heap, int i) {
+		int highest = i;
+		int left = i*2;
+		int right = (i*2)+1;
+		if(left<=n1 && heap[left-1]>heap[highest-1]) {
+			highest=left;
+		}
+		
+		if(right<=n1 && heap[right-1]>heap[highest-1]) {
+			highest=right;
+		}
+		if(highest!=i) {
+			swap(heap, highest-1, i-1);
+			heapify(heap, highest);
+		}
+		
+	}
+	
+	public static void buildHeap(int[] arr) {
+		// leaf node of complete exist from index i = n/2+1 to n, where n is the number elements in tree
+		for(int i=n1/2; i>0; i--) {
+			heapify(arr, i);
+		}
+	}
+	
+	public static void delete(int[] heap) {
+		heap[0]=heap[n1-1];
+		n1--;
+		heap[n1]=0;
+		heapify(heap, 1);
 	}
 
 	public static void printHeap(int[] heap) {
@@ -81,4 +110,16 @@ public class Heap {
 			p *= 2;
 		}
 	}
+	
+	public static void heapSort(int[] heap1) {
+		buildHeap(heap1);
+		int temp = n1;
+		for(int i=n1; i>1; i--) {
+			swap(heap1, i-1, 0);
+			n1--;
+			heapify(heap1, 1);
+		}
+		n1 = temp;
+	}
+	
 }
