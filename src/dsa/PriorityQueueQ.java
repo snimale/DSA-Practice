5423491fca5bec2fa92975e00e450b690b6b92b2
Soham Nimale <soham.sachin.nimale@gmail.com>
2022-11-02 18:57:00 +0530

    added easy Q - PriorityQueueQ - find kth smallest num in arr

--- src/dsa/PriorityQueueQ.java
+++ src/dsa/PriorityQueueQ.java
@@ -5,23 +5,40 @@ import java.util.*;
 public class PriorityQueueQ {
 	public static void main(String args[]) {
 		
-		// find the k'th largest element
-		int[] a = {1, 6, 7, 3, 4, 1, 4, 2, 6, 3};
-		System.out.println(kthLargest(a, 2));
+
+		int[] a = {1, 10, 15, 40, 9, 20, 77, 4, 55};
+		System.out.println(kthsmallest(a, 2));
 	
 	}
 	
+	// find the k'th largest element
 	public static int kthLargest(int[] arr, int k) {
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		PriorityQueue<Integer> pq1 = new PriorityQueue<>();
 		for(int i=0; i<k; i++) {
-			pq.add(arr[i]);
+			pq1.add(arr[i]);
 		}
 		for(int i=k; i<arr.length; i++) {
-			if(pq.peek()<arr[i]) {
-				pq.poll();
-				pq.add(arr[i]);
+			if(pq1.peek()<arr[i]) {
+				pq1.poll();
+				pq1.add(arr[i]);
 			}
 		}
-		return pq.peek();
+		return pq1.peek();
+	}
+	
+	// find the k'th smallest element
+	public static int kthsmallest(int[] arr, int k) {
+		PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+		for(int i=0; i<k; i++) {
+			pq2.add(arr[i]);
+		}
+		
+		for(int i=k; i<arr.length; i++) {
+			if(pq2.peek()>arr[i]) {
+				pq2.poll();
+				pq2.add(arr[i]);
+			}
+		}
+		return pq2.peek();
 	}
 }
