bacde4d551a537827521aa463d45b71b8fe06de3
Soham Nimale <soham.sachin.nimale@gmail.com>
2022-11-15 19:02:34 +0530

    added hard Q - to find next smaller element of current element in array in 0(N) time

--- src/hardQ/StackQ.java
+++ src/hardQ/StackQ.java
@@ -3,26 +3,47 @@ package hardQ;
 import java.util.*;
 public class StackQ {
 	public static void main(String args[]) {
-		// To find previous smaller number/ its index i.e nearest small number w.r.t current number in array
-		// Time Complexity - O(N) as we push and pop one element one time only
 		int[] arrQ = {4, 5, 1, 3, 4, 2, 5, 9, 5, 2, 3, 1};
-		int[] arrA = new int[arrQ.length];
-		Deque<Integer> temp = new ArrayDeque<>(arrQ.length);
-		
+		int[] ans1=psn(arrQ);
+		int[] ans2=nsn(arrQ);
+		System.out.println(Arrays.toString(ans1)+"\n"+Arrays.toString(ans2));
+	}
+	// To find previous smaller number/ its index i.e nearest small number on left side w.r.t current number in array
+	// Time Complexity - O(N) as we push and pop one element one time only
+	public static int[] psn(int[] arrQ) {
+		int[] arrA1 = new int[arrQ.length];
+		Deque<Integer> temp1 = new ArrayDeque<>(arrQ.length);
 		for(int i=0; i<arrQ.length; i++) {
-			while(!temp.isEmpty() && arrQ[temp.peek()]>=arrQ[i]) {
-				temp.pop();
+			while(!temp1.isEmpty() && arrQ[temp1.peek()]>=arrQ[i]) {
+				temp1.pop();
 			}
-			if(temp.isEmpty()) {
-				arrA[i]=-1;
+			if(temp1.isEmpty()) {
+				arrA1[i]=-1;
 			} else {
-				arrA[i]=temp.peek();
+				arrA1[i]=temp1.peek();
 			}
-			temp.push(i);
+			temp1.push(i);
 		}
-		
-		for(int i : arrA) {
-			System.out.print(i+", ");
+		return arrA1;
+	}
+	
+	
+	// To find next smaller number/ its index i.e nearest small number on right side w.r.t current number in array
+	// Time Complexity - O(N) as we push and pop one element one time only
+	public static int [] nsn(int[] arrQ) {
+		int[] arrA2 = new int[arrQ.length];
+		Deque<Integer> temp2 = new ArrayDeque<>(arrQ.length);
+		for(int i=arrQ.length-1; i>=0; i--) {
+			while(!temp2.isEmpty() && arrQ[temp2.peek()]>=arrQ[i]) {
+				temp2.poll();
+			}
+			if(temp2.isEmpty()) {
+				arrA2[i]=arrQ.length;
+			} else {
+				arrA2[i]=temp2.peek();
+			}
+			temp2.push(i);
 		}
+		return arrA2;
 	}
 }
